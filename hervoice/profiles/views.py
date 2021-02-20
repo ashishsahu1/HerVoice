4b2e73b1968b1f4d95e818433905737c9e53194e
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-02-20 15:46:33 +0530

    stopper 1

--- hervoice/profiles/views.py
+++ hervoice/profiles/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Profile, Relationship
+# from .forms import ProfileModelForm
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+
+
+def my_profile_view(request):
+    pass
